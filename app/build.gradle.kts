2ea6f0475fea00318ccffce6fa30801b47193083
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2024-04-08 02:17:40 +0200

    08/04/2024
    - Se arregló que el comportamiento que realiza el registro de usuario, actualizando la información del usuario nuevo, recogiendo los datos de la BBDD una vez se ha añadido a la misma, mostrando en ajustes el valor inicial de los GloryCoins a 1 tal y como está establecido en la creación de cada usuario.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 25
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "Developers Version (pre-alpha 1.0)"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/glorystarcompany/tfgdreamteam/SecondActivity.java
+++ app/src/main/java/com/glorystarcompany/tfgdreamteam/SecondActivity.java
@@ -86,6 +86,13 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
                     usuarioNuevo = new Usuario(newUserName, newUserPassWord, newUserMail);
 
                     firstActivityBD.insertUsuario(usuarioNuevo);
+
+                    for (int i = 0; i < firstActivityBD.getUsuarios().size(); i++) {
+                        if (usuarioNuevo.getName().equals(firstActivityBD.getUsuarios().get(i).getName())){
+                         usuarioNuevo = firstActivityBD.getUsuarios().get(i);
+                        }
+                    }
+
                     signUpMessage = "Usuario creado. Accediendo...";
                     showToast(signUpMessage);
                     Intent goToThirdActivity = new Intent(this, ThirdActivity.class);
